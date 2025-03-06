30962cfd2f79d60718a233c3660c23d0baf5b4a4
Miha Krajnc <miha@krajnc.dev>
2025-03-06 03:26:16 +0100

    feat: add example prowlarr page

--- Cheesarr/Services/ProwlarrService.cs
+++ Cheesarr/Services/ProwlarrService.cs
@@ -3,20 +3,20 @@ using Cheesarr.Settings;
 
 namespace Cheesarr.Services;
 
-public class ProwlarrService(HttpClient httpClient, SettingsService settings, ILogger<OpenLibraryService> logger)
+public class ProwlarrService(HttpClient httpClient, ILogger<OpenLibraryService> logger)
 {
-    private const string SEARCH_API_URL = "search?query={0}";
+    private const string SEARCH_API_URL = "api/v1/search?query={0}";
 
     private const int CAT_EBOOKS = 7020;
     private const int CAT_AUDIOBOOKS = 3030;
 
-    public Task<ProwlarrItem[]> Search(string bookName, bool ebooks, bool audiobooks)
+    public Task<List<ProwlarrItem>> Search(string bookName, bool ebooks, bool audiobooks)
     {
         var url = string.Format(SEARCH_API_URL, Uri.EscapeDataString(bookName));
 
         if (ebooks) url += $"&categories={CAT_EBOOKS}";
         if (audiobooks) url += $"&categories={CAT_AUDIOBOOKS}";
 
-        return httpClient.GetFromJsonAsync<ProwlarrItem[]>(url)!;
+        return httpClient.GetFromJsonAsync<List<ProwlarrItem>>(url)!;
     }
 }
